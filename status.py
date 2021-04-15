62440e82df4ae64fa5a66bc149745a49c62acf32
zanderp25 <zanderpicon@outlook.com>
2021-04-15 16:46:08 -0400

    fetch_user not get_user

--- status.py
+++ status.py
@@ -22,7 +22,7 @@ class Status(commands.Cog):
             )
         )
     async def notify(self,user):
-        users = [(await self.bot.fetch_channel(config.notif_channel)).guild.get_user(user).mention for user in config.notif_users]
+        users = [(await(await self.bot.fetch_channel(config.notif_channel)).guild.fetch_user(user)).mention for user in config.notif_users]
         await (await self.bot.fetch_channel(config.notif_channel)).send(
             f'{", ".join(users)} {user.name} is offline'
         )
