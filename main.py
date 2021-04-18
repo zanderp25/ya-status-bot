0f3a4a5fd1e98c61b22de9052ea20fc88649a2d1
EEKIM10 <eek@clicksminuteper.net>
2021-04-18 15:10:52 +0100

    oh my god the formatting

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import discord, config
 from discord.ext import commands
 
 bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("yas!","//"), 
+    command_prefix=commands.when_mentioned_or("yas!", "//"),
     intents=discord.Intents.all(),
-    owner_ids=[511655498676699136,421698654189912064],
-    )
+    owner_ids=[511655498676699136, 421698654189912064],
+)
 token = open("token.txt").read()
+
 
 @bot.event
 async def on_ready():
@@ -16,12 +17,13 @@ async def on_ready():
     print(f"{user} is now {'offline' if not user.status == discord.Status.online else 'online'}")
     await bot.change_presence(
         activity=discord.Activity(
-            type=discord.ActivityType.watching, 
+            type=discord.ActivityType.watching,
             name=f"{user.name} be {'offline' if not user.status == discord.Status.online else 'online'}",
         ),
     )
 
-cogs = ["jishaku","status"]
+
+cogs = ["jishaku", "status"]
 
 for cog in cogs:
     try:
--- status.py
+++ status.py
@@ -1,54 +1,55 @@
 import discord, config, asyncio
 from discord.ext import commands
 
+
 class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def set_status(self,user):
+    async def set_status(self, user):
         await self.bot.change_presence(
             activity=discord.Activity(
-                type=discord.ActivityType.watching, 
+                type=discord.ActivityType.watching,
                 name=f"{user.name} be {'offline' if not user.status == discord.Status.online else 'online'}",
             ),
         )
         await (await self.bot.fetch_channel(config.channel)).send(
             embed=discord.Embed(
-                color = discord.Color.red() if not user.status == discord.Status.online else discord.Color.green()
+                color=discord.Color.red() if not user.status == discord.Status.online else discord.Color.green()
             ).set_author(
-                name=f"{user.name} is now {'offline' if not user.status == discord.Status.online else 'online'}", 
-                icon_url=f"{user.avatar_url}"
+                name=f"{user.name} is now {'offline' if not user.status == discord.Status.online else 'online'}",
+                icon_url=f"{user.avatar_url}",
             )
-        )
-    async def notify(self,user):
-        users = [(await self.bot.fetch_channel(config.notif_channel)).guild.get_member(user).mention for user in config.notif_users]
-        await (await self.bot.fetch_channel(config.notif_channel)).send(
-            f'{", ".join(users)}: {user.name} is offline'
         )
 
+    async def notify(self, user):
+        users = [
+            (await self.bot.fetch_channel(config.notif_channel)).guild.get_member(user).mention
+            for user in config.notif_users
+        ]
+        await (await self.bot.fetch_channel(config.notif_channel)).send(f'{", ".join(users)}: {user.name} is offline')
+
     @commands.command()
-    async def status(self,ctx):
-        '''tests the status of the bot/user currently being tracked'''
+    async def status(self, ctx):
+        """tests the status of the bot/user currently being tracked"""
         user = (await self.bot.fetch_channel(config.channel)).guild.get_member(config.user)
         await ctx.reply(
-            embed = discord.Embed(
+            embed=discord.Embed(
                 description=f"{user.name} is {'offline' if not user.status == discord.Status.online else 'online'} right now.",
-                color = discord.Color.red() if not user.status == discord.Status.online else discord.Color.green()
-            ).set_author(
-                name=f"{user.name}", 
-                icon_url=f"{user.avatar_url}"
-            )
+                color=discord.Color.red() if not user.status == discord.Status.online else discord.Color.green(),
+            ).set_author(name=f"{user.name}", icon_url=f"{user.avatar_url}")
         )
-    @commands.command(name="notif-test", aliases=["notif","notiftest"])
+
+    @commands.command(name="notif-test", aliases=["notif", "notiftest"])
     @commands.is_owner()
-    async def test_notify(self,ctx):
-        '''tests if the notifications work'''
+    async def test_notify(self, ctx):
+        """tests if the notifications work"""
         await self.notify(ctx.author)
         await ctx.reply("test notification sent")
 
-    @commands.command(name="clear",aliases=["purge"])
+    @commands.command(name="clear", aliases=["purge"])
     @commands.has_permissions(manage_messages="true")
-    async def clear(self,ctx):
+    async def clear(self, ctx):
         if ctx.channel.id == config.channel:
             n = await ctx.channel.purge(
                 limit=1000,
@@ -58,10 +59,12 @@ class Status(commands.Cog):
             await asyncio.sleep(2)
             await msg.delete()
         else:
-            await ctx.reply(f"This command can only be used in {(await self.bot.fetch_channel(config.channel)).mention}")
-    
+            await ctx.reply(
+                f"This command can only be used in {(await self.bot.fetch_channel(config.channel)).mention}"
+            )
+
     @commands.Cog.listener(name="on_member_update")
-    async def on_member_update(self,before,after):
+    async def on_member_update(self, before, after):
         if before.id == config.user:
             if before.guild == (await self.bot.fetch_channel(config.channel)).guild:
                 if before.status == discord.Status.online and after.status != discord.Status.online:
@@ -71,7 +74,7 @@ class Status(commands.Cog):
                 elif before.status != discord.Status.online and after.status == discord.Status.online:
                     print("Online!")
                     await self.set_status(after)
-            
+
 
 def setup(bot):
     bot.add_cog(Status(bot))
