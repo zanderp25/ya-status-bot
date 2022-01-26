4097503e01534620d64c5923331f381712b99eb8
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-01-26 13:31:40 -0500

    removed eek from the mention list

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ channel = 738001095980220496
 
 # channel and users to send notifications
 notif_channel = 706301766832095312
-notif_users = [421698654189912064, 511655498676699136]
+notif_users = [511655498676699136]
 
 # bot owner IDs
 # If you want to use the discord teams feature, please set this to `None`
